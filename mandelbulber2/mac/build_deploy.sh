6cb67ad453a344b4f50be1390557a815b964aa4b
Coastrnaut <rpancoast77@gmail.com>
2026-08-22 09:47:37 -0400

    Update build_deploy.sh

--- mandelbulber2/mac/build_deploy.sh
+++ mandelbulber2/mac/build_deploy.sh
@@ -16,13 +16,20 @@ if ls /usr/local/bin/ | grep brew > /dev/null ; then tput sgr0 ; echo "HomeBrew 
 brew update
 brew install llvm qt gsl lzo jpeg libpng libtiff libsndfile tree p7zip wget
 
-# adam - Fix OpenCL
-mkdir -p  ~/mandelbulber2/build/OpenCL
-git clone https://github.com/KhronosGroup/OpenCL-CLHPP.git ~/mandelbulber2/build/OpenCL
-cp -vfr ~/mandelbulber2/build/OpenCL/include/CL/ ~/mandelbulber2/build/OpenCL
+# 1. Clone the repository into a clean local directory
+CLONE_DIR="$BUILD/OpenCL-CLHPP-Repo"
+git clone https://github.com/KhronosGroup/OpenCL-CLHPP.git "$CLONE_DIR"
+# 2. Build the exact OpenCL folder structure required by Mandelbulber Mac profiles
+mkdir -p "$BUILD/OpenCL"
+# 3. Copy the base contents safely into the folder
+cp -vfr "$CLONE_DIR/include/CL/"* "$BUILD/OpenCL/"
+# 4. Generate the missing cl.hpp file name mapping using the universal source base
+cp -vf "$BUILD/OpenCL/opencl.hpp" "$BUILD/OpenCL/cl.hpp"
 
 # build operation
-cd $BUILD && qmake $SRC/mandelbulber2/qmake/mandelbulber-opencl-mac.pro
+cd $BUILD && qmake $SRC/mandelbulber2/qmake/mandelbulber-opencl-mac.pro \
+QMAKE_CXXFLAGS+="-I$BUILD -DCL_SILENCE_DEPRECATION -DCL_TARGET_OPENCL_VERSION=120" \
+QMAKE_CFLAGS+="-I$BUILD -DCL_SILENCE_DEPRECATION -DCL_TARGET_OPENCL_VERSION=120"
 cd $BUILD && make -j8
 
 # binary requirement
@@ -55,7 +62,7 @@ mkdir -vp "$PACK/doc"
 cp -v "$SUPPORT/deploy/NEWS" "$PACK/doc"
 DOCFILE="$(curl -s https://api.github.com/repos/buddhi1980/mandelbulber_doc/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4)"
 echo $DOCFILE
-wget -O "$SHARE/doc/Mandelbulber_Manual.pdf" $DOCFILE
+wget -O "$PACK/doc/Mandelbulber_Manual.pdf" $DOCFILE
 #copy c++abi
 mkdir -p "$PACK/Contents/Frameworks/"
 cp "/usr/lib/libc++abi.dylib" "$PACK/Contents/Frameworks/"
